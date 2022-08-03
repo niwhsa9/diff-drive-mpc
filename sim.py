correct display of rotation

sim.py
@@ -36,15 +36,16 @@ class Sim:
         return world_dims * np.array(self.screen_dims) / np.array(self.world_dims)
 
     def draw(self, screen: pygame.surface.Surface) -> None:
+        # Blank background
         screen.fill((0, 0, 0))
 
         # Display the robot
         robot_pos, robot_theta = robot.get_drawable()
-        hitbox = self.robot_sprite.get_rect()
-        hitbox.center = tuple(self.world_to_screen_pos(robot_pos))
-        #rotated_hitbox = pygame.transform.rotate(
-        screen.blit(self.robot_sprite, hitbox)
+        robot_sprite = pygame.transform.rotate(self.robot_sprite, np.rad2deg(robot_theta))
+        hitbox = robot_sprite.get_rect(center=tuple(self.world_to_screen_pos(robot_pos)))
+        screen.blit(robot_sprite, hitbox)
 
+        # Flip buffers to draw graphics
         pygame.display.flip()
 
     def run(self):
@@ -55,8 +56,9 @@ class Sim:
             pygame.image.load("resources/arrow.jpg"),
             self.world_to_screen_dims(self.robot_size),
         )
-        # self.robot_sprite.convert()
-        # self.robot_sprite.set_color_key((0, 0, 0))
+        # Necessary for alpha
+        self.robot_sprite.convert()
+        self.robot_sprite.set_colorkey((0, 0, 0))
 
         running: bool = True
 
@@ -85,7 +87,7 @@ class Sim:
 
 
 if __name__ == "__main__":
-    robot = UnicycleKinematics(np.array([0, 0, 0]))
+    robot = UnicycleKinematics(np.array([0, 0, np.pi/4]))
     controller = DummyController()
     sim = Sim(240, 60, robot, controller)
     sim.run()