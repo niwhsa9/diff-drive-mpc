d2faa27f6089f1fcc9f8bc5e675df8df4baf8f0d
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-08-04 18:24:09 -0400

    construct prediction matricies

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Tuple
 
-from robot import LinearRobot
+from robot import LinearRobot, LinearUnicycleKinematics
 
 import numpy as np
 from scipy.interpolate import interp1d
@@ -64,28 +64,38 @@ class PoseMPC(Controller):
         self.horizon = horizon 
         self.dt = dt
 
-
+    # TODO remove list comprehensions for matrix powers
     def get_control(self, x: np.ndarray, t: float) -> np.ndarray:
+
         # Get the reference trajectory samples over the horizon
-        x_r = traj.get_poses_lookahead(t, self.horizon, self.dt)
+        x_r = self.traj.get_poses_lookahead(t, self.horizon, self.dt)
         N = x_r.shape[0]
+
         # Get the Jacobians of the model at the current state
         model : LinearRobot = LinearUnicycleKinematics.from_dt(x, self.dt)
         A = model.A
         B = model.B
-        # Determine the matricies S and R that allow us to compute the
+
+        # Determine the matricies S and R that allow us to roll out the
         # predicted trajectory x over the horzion  
         # x = S x_t + R u_t:t+N 
-        S = np.linalg.matrix_power(A, n) 
+        S = np.array( [np.linalg.matrix_power(A, n) for n in range(1, N+1)] ) # N x 3 x 3
+        alpha = S @ x # N x 3
         # Compute the highest order row for final predicted state 
         R_nm1_b = np.tile(B, (N, 1, 1))
-        # TODO Is this doable in a vectorized way?
         R_nm1_a = np.array([ np.linalg.matrix_power(A, i) for i in range(N)])
-        R_nm1 = A @ B
-        R = np.tile(R_nm1, (N, 1))
-        #R = [np.roll(R, 
-        #R = 
-        # mask R to be triangular
+        R_nm1 = R_nm1_a @ R_nm1_b # N x 2 x 2
+        R_nm1 = R_nm1.reshape(N*2, -1).T # 2 x (2)(N)
+        # Build the R matrix to get predictions at each step
+        R = np.tile(R_nm1, (N, 1)) # (2)(N) x (2)(N)
+        R = np.array([np.roll(row, x) for row, x in zip(R, np.arange(N)[::-1].repeat(2))])
+        # Build block triangular mask and apply
+        d = R.shape[0]//2
+        mask = np.repeat(np.repeat(np.tril(np.ones((d, d))), 2, 1), 2, 0)
+        R = R * mask
+
+        # Formulate the QP in canonical form
+
         return np.array([0, 0])
 
 
