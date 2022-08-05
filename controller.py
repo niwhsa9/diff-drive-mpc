35c7b99e44d554393d334dec70ce94eaf24b5240
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-08-04 22:40:33 -0400

    change rollout scheme

--- controller.py
+++ controller.py
@@ -37,7 +37,7 @@ class Trajectory:
         )
 
     def get_poses_lookahead(
-        self, start_time: float, duration: float, dt: float
+        self, start_time: float, N: float, dt: float
     ) -> np.ndarray:
         """
         Retrieves the poses of the trajectory from the start time to
@@ -46,7 +46,7 @@ class Trajectory:
         trajectory.
 
         """
-        sample_times = np.arange(start_time, start_time + duration, dt)
+        sample_times = np.arange(0, N)*dt + start_time
         return self.interp(sample_times).T
 
 
@@ -61,23 +61,22 @@ class PoseMPC(Controller):
         traj - Trajectory you want to track
         Q - positive semi-definite matrix for quadratic cost in position
         """
-        N = int(np.floor(horizon/dt))
+        self.N = int(np.floor(horizon/dt))
         self.traj = traj
-        self.Q = block_diag(*[Q for _ in range(N)])
+        self.Q = block_diag(*[Q for _ in range(self.N)])
         self.horizon = horizon 
         self.dt = dt
         self.max_vel = max_vel
         self.prev_u = np.zeros(2)
 
-        self.G = np.vstack((np.eye(N*2),np.eye(N*2)))
-        self.h = np.tile(self.max_vel, N*2)
+        self.G = np.vstack((np.eye(self.N*2),np.eye(self.N*2)))
+        self.h = np.tile(self.max_vel, self.N*2)
 
     # TODO vectorize list comprehensions for matrix powers
     def get_control(self, x: np.ndarray, t: float) -> np.ndarray:
 
         # Get the reference trajectory samples over the horizon
-        x_r = self.traj.get_poses_lookahead(t, self.horizon, self.dt) # N x 3
-        N = x_r.shape[0]
+        x_r = self.traj.get_poses_lookahead(t, self.N, self.dt) # N x 3
 
         # Get the Jacobians of the model at the current state
         model : LinearRobot = LinearUnicycleKinematics.from_dt(x, self.prev_u, self.dt)
@@ -87,18 +86,18 @@ class PoseMPC(Controller):
         # Determine the matricies S and R that allow us to roll out the
         # predicted trajectory x over the horzion  
         # x = S x_t + R u_t:t+N 
-        S = np.array( [np.linalg.matrix_power(A, n) for n in range(1, N+1)] ) # N x 3 x 3
+        S = np.array( [np.linalg.matrix_power(A, n) for n in range(1, self.N+1)] ) # N x 3 x 3
         alpha = S @ x # N x 3
         # Compute the highest order row for final predicted state 
-        R_nm1_b = np.tile(B, (N, 1, 1))
-        R_nm1_a = np.array([ np.linalg.matrix_power(A, i) for i in range(N)])
+        R_nm1_b = np.tile(B, (self.N, 1, 1))
+        R_nm1_a = np.array([ np.linalg.matrix_power(A, i) for i in range(self.N)])
         R_nm1 = R_nm1_a @ R_nm1_b # N x 3 x 2
-        R_nm1 = R_nm1.reshape(N*2, -1).T # 3 x (2)(N)
+        R_nm1 = R_nm1.reshape(self.N*2, -1).T # 3 x (2)(N)
         # Build the R matrix to get predictions at each step
-        R = np.tile(R_nm1, (N, 1)) # (3)(N) x (2)(N)
-        R = np.array([np.roll(row, x) for row, x in zip(R, -np.arange(N)[::-1].repeat(3))])
+        R = np.tile(R_nm1, (self.N, 1)) # (3)(N) x (2)(N)
+        R = np.array([np.roll(row, x) for row, x in zip(R, -np.arange(self.N)[::-1].repeat(3))])
         # Build block triangular mask and apply
-        mask = np.repeat(np.repeat(np.tril(np.ones((N, N))), 2, 1), 3, 0)
+        mask = np.repeat(np.repeat(np.tril(np.ones((self.N, self.N))), 2, 1), 3, 0)
         R = R * mask
 
         # Formulate the QP in canonical form 
